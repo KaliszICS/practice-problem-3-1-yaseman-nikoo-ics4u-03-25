da60ec93faead3a953963b994747e17e4f154acc
yaseman-nikoo-ics4u-03-25 <yaseman.nikoo@student.tdsb.on.ca>
2025-04-02 18:54:26 +0000

    complete 3.1

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -3,7 +3,26 @@ public class PracticeProblem {
 	public static void main(String args[]) {
 
 	}
+	public static int sumOfDigits(int x){
+		if (x<0){
+			x=-x;
+		}
+		if(0<=x && x<=9){
+			return x;
+		}
+		return sumOfDigits(x/10)+(x%10);
+		}
+	public static int factorial(int n){
+		if (n==0){
+			return 1;
+		}
+		else if(n<0){
+			return 0;
+		}
+		return factorial(n-1)*n;
+	}
 
+	}
 	
 
-}
+
